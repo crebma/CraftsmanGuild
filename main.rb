427fb0ac4d045f3f7b068f44bda9bf0ae4a621a0
Amber Conville <crebma@gmail.com>
2013-03-17 21:16:45 -0400

    making everything more dynamic!

--- main.rb
+++ main.rb
@@ -1,6 +1,42 @@
 require 'sinatra'
+require 'data_mapper'
+require 'json'
+require 'net/https'
+
+DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_MAROON_URL'] || 'postgres://postgres:password@localhost:5432/cg')
+
+class Event
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :title, String, :length => 256
+  property :description, String, :length => 1024
+  property :date, Date
+  property :event_link, String, :length => 256
+
+  has n, :presenters
+
+  def self.current
+    first(:date.gt => Date.today.prev_day)
+  end
+
+end
+
+class Presenter
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+  property :bio, String, :length => 1024
+  property :twitter, String
+
+  belongs_to :event
+end
+
+DataMapper.finalize.auto_upgrade!
 
 get '/' do
+  @event = Event.current
   erb :index
 end
 
@@ -9,7 +45,13 @@ get '/map' do
 end
 
 get '/events' do
+  @events = Event.all(:date.lt => Date.today.prev_day, :order => [ :date.desc ])
   erb :events
+
+end
+
+get '/presenters' do
+  JSON.generate Presenter.all()
 end
 
 get '/resources' do
